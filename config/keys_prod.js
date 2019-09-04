ec82e55a66a2c4edf670e90fcb7ec6e755bab918
Surya <suriyahh2006@gmail.com>
2019-09-04 16:25:14 +0100

    updated

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+    // mongoURI: 'mongodb://127.0.0.1:27017/admin',
+    mongoURI: 'mongodb+srv://suriyahh2006:Rajesh87@mongocluster1-l6mpp.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'Testraid'
 };
